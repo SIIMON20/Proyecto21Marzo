6c961ad71b3ab89a19d38b4d5c759281e8bc2e28
FerneyMurillo28 <ferneymurillo044@gmail.com>
2023-03-21 12:32:04 -0500

    Mensaje

---
+++ app/src/main/java/com/example/proyecto25febrero/Screens/Peliculas.kt
@@ -0,0 +1,34 @@
+package com.example.proyecto25febrero.Screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun SecondBodyContent(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "Segunda pagina")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Regresar")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+
+@Composable
+
+fun PreviewSecondBodyContent(){
+    SecondBodyContent()
+}
